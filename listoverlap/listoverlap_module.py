f8bab66f13e5b7bf78a49c6019323fad37365f87
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-06-19 14:07:08 +0200

    listoverlap_module is ready to use without randomly generating lists

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,9 +1,11 @@
 def listoverlap(list1, list2):
-    return
+    return list(set(list1) & set(list2))
 
 
 def main():
-    return
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    print(listoverlap(a, b))
 
 
 if __name__ == '__main__':
